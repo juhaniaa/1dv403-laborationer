7dd5533d148104d61564e49f0e27a14bc56528c5
Juhani <ja222qm@lnu.student.se>
2013-11-27 13:26:27 +0200

    Användaren kan skicka meddelanden med Enter + radera meddelanden

--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -6,6 +6,29 @@ var MessageBoard = {
         
         var send = document.getElementById("send");
         send.onclick = MessageBoard.writeMessage;
+        
+        // meddelandet ska kunna skickas med enter tangenten
+        var textField = document.getElementById("textArea");
+        console.log(textField);
+
+        textField.onkeypress = function (e) {
+            // IE fix
+            if (!e) { var e = window.event };
+
+            var fieldValue = document.getElementById("textfield").getElementsByTagName("textarea")[0].value;
+
+            // om shift knappen inte är ned tryckt...
+            if (!e.shiftKey) {
+
+                // ...och användaren trycker på enter...
+                if (e.keyCode === 13) {
+
+                    //...så skall meddelandet "skickas"
+                    MessageBoard.writeMessage();
+                    return false;
+                }
+            }
+        }
     },
     
     writeMessage: function () {
@@ -38,6 +61,36 @@ var MessageBoard = {
         var messageTime = document.createElement("span");
         messageTime.innerHTML = MessageBoard.messages[messageID].getDateText();
         text.appendChild(messageTime);
+        
+        // uppdatera meddelande räknaren
+        MessageBoard.messageCount();
+        
+        // skapa en länk för att radera ett meddelande
+        var aClose = document.createElement("a");
+        aClose.href = "#";
+        aClose.alt = "Close Message";
+        aClose.className = "aClose";
+        text.insertBefore(aClose, text.firstChild);
+        
+        aClose.onclick = function () {
+
+            if (window.confirm("Vill du verkligen radera meddelandet?")) {
+                MessageBoard.removeMessage(messageID);
+            }
+            return false;
+        };
+        
+        // skapa en länk för att se tidsstämpel för meddelandet
+        var aTime = document.createElement("a");
+        aTime.href = "#";
+        aTime.alt = "Message creation time";
+        aTime.className = "aTime";
+        text.insertBefore(aTime, aClose);
+        
+        aTime.onclick = function () {
+            alert("Inlägget skapades den " + MessageBoard.messages[messageID].getAlertText() + " klockan " + MessageBoard.messages[messageID].getDateText());
+            return false;
+        };
     
     },
     
@@ -47,6 +100,20 @@ var MessageBoard = {
         
         // anropa renderMessage för varje meddelande i messages arrayen
         MessageBoard.messages.forEach(MessageBoard.renderMessage);
+        
+        // då det inte finns några meddelanden att skrivas ut uppdateras endast antalet meddelanden
+        if (MessageBoard.messages.length === 0) {
+            MessageBoard.messageCount();
+        }
+    },
+    
+    messageCount: function () {
+        document.getElementById("textfield").getElementsByTagName("span")[0].innerHTML = MessageBoard.messages.length;
+    },
+    
+    removeMessage: function (messID) {
+        MessageBoard.messages.splice(messID, 1);
+        MessageBoard.renderMessages();
     }
 };
 
