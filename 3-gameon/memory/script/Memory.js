9393adf7157e87017d353f15bd9d72f969a130e1
Juhani <ja222qm@lnu.student.se>
2013-12-04 11:15:20 +0200

    Bilderna vänds då användaren klickar på dem

--- 3-gameon/memory/script/Memory.js
+++ 3-gameon/memory/script/Memory.js
@@ -1,6 +1,8 @@
 function Memory(rows, cols, gameId) {
     "use strict";
     
+    var cssPosition = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight"];
+    
     // skapa en div tag med gameId för spelbrädan
     var div = document.createElement("div");
     div.id = gameId;
@@ -15,6 +17,13 @@ function Memory(rows, cols, gameId) {
     div.appendChild(table);
     
     function createTable(element, index, array) {
+        
+        // vänder den bild som användaren tryckt på
+        function turnPic() {
+            this.className = cssPosition[element];
+            return false;
+        }
+        
         //då index + 1 % 3 == 0 skapas en ny tr som läggs till i table
         if (index % 3 == 0) {
             console.log(index);
@@ -22,15 +31,20 @@ function Memory(rows, cols, gameId) {
         }
         
         // sedan skapas en ny td för varje element och läggs till i tr
-        // tdn skall innehålla en a tag som har en css klass innehållandes arrayens värde
-        // utifrån css bestäms vilken bild som hamnar i de olika a taggarna
+        // tdn skall innehålla en a tag som har en css klass för nedvänd bild
+        // a taggen skall även ha en onklick som anropar turnPic
         var td = document.createElement("td");
         var a = document.createElement("a");
-        a.className = element;
+        a.className = "zero";
         a.href = "#";
+        a.onclick = turnPic;
         td.appendChild(a);
-        a.innerHTML = '<img src="pics/0.png" >';
         table.lastChild.appendChild(td);
+        
+        
     }
     rndArray.forEach(createTable);
+    
+
 }
+
