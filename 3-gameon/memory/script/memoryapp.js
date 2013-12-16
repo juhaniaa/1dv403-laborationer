b9583d8debf50c6dd592858351930ee70a54df3b
Juhani <ja222qm@lnu.student.se>
2013-12-16 08:37:38 +0200

    fler spel

--- 3-gameon/memory/script/memoryapp.js
+++ 3-gameon/memory/script/memoryapp.js
@@ -4,7 +4,8 @@ var MemoryApp = {
         var mem1 = new Memory(3, 4, "game1");
         var mem2 = new Memory(2, 5, "game2");
         var mem2 = new Memory(4, 2, "game3");
-        var mem2 = new Memory(5, 3, "game4");
+        var mem2 = new Memory(5, 2, "game4");
+        var mem2 = new Memory(2, 3, "game5");
     }
 };
 
