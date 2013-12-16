2e8da8d62c106aa5eeba4bf9b67e1ca61ea20048
Juhani <ja222qm@lnu.student.se>
2013-12-16 10:57:46 +0200

    namn ej tomt samt postnr validering

--- 4-forms/js/validering.js
+++ 4-forms/js/validering.js
@@ -8,26 +8,55 @@ var Validator = {
         firstName.onfocus = function () {
             this.select();
         };
+        
         firstName.onchange = function () {
-            var fnPattern = /^[a-zA-Z]+$/;
+            var fnPattern = /^.+$/;
             if (!firstName.value.match(fnPattern)) {
-                alert("får ej vara tom!!!");
-                var p = document.createElement("p");
-                p.innerHTML = "Detta fält får inte vara tomt!";
+                this.previousElementSibling.textContent = "Förnamn: Får inte vara tomt!";
+                this.previousElementSibling.className = "notCorrect";
                 return false;
             } else {
-                console.log("ändra tillbaka till skicka");
+                this.previousElementSibling.textContent = "Förnamn:";
+                this.previousElementSibling.className = "normal";
             }
         };
         
         var lastName = form.elements.efternamn;
-        firstName.onblur = function () {};
+        lastName.onfocus = function () {
+            this.select();
+        };
+        
+        lastName.onchange = function () {
+            var lnPattern = /^.+$/;
+            if (!lastName.value.match(lnPattern)) {
+                this.previousElementSibling.textContent = "Efternamn: Får inte vara tomt!";
+                this.previousElementSibling.className = "notCorrect";
+                return false;
+            } else {
+                this.previousElementSibling.textContent = "Efternamn:";
+                this.previousElementSibling.className = "normal";
+            }
+        };
         
         var postNr = form.elements.postnr;
-        postNr.onblur = function () {};
+        postNr.onfocus = function () {
+            this.select();
+        };
+        postNr.onchange = function () {
+            var postPattern = /^(SE)?\s?\d{3}(\s|-)?\d{2}$/;
+            if (!postNr.value.match(postPattern)) {
+                this.previousElementSibling.textContent = "Postnummer: Bör vara i 'XXXXX' format";
+                this.previousElementSibling.className = "notCorrect";
+                return false;
+            } else {
+                this.previousElementSibling.textContent = "Postnummer:";
+                this.previousElementSibling.className = "normal";
+            }
+        
+        };
         
         var email = form.elements.epost;
-        email.onblur = function () {};
+        email.onchange = function () {};
     }
 };
 
